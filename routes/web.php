0a06c5bea5e0112502dd0310b503d579bc5cfa60
Thirsya <thirsya.widya@gmail.com>
2022-02-23 15:41:25 +0700

    Praktikum 2 Nomor 5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
